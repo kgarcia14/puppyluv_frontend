00ff976b46dede7b24aa8ad29ca3ac279656c98d
Kurtis Garcia <kurtisgarcia14@gmail.com>
2021-03-26 12:02:56 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
-import NavBar from "./components/NavBar";
 import './App.css';
 import LoginButton from './components/LoginButton'
 import Logout from './components/LogoutButton';
 import Profile from './components/Profile';
+import Navbar from './components/Navbar';
 
 
 function App() {
