11a6141e7cbe90010f02ef400ea8fec76aefdaa7
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-03-25 20:52:40 -0400

    Added Auth0 for user authorization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
+import LoginButton from './components/LoginButton'
+import Logout from './components/LogoutButton';
+import Profile from './components/Profile';
+
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Nav-links">
+          <LoginButton />
+          <Logout />
+        </div>
+        <h1>Puppy Luv</h1>
       </header>
+      <Profile />
     </div>
   );
 }
