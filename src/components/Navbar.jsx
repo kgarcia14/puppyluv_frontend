856f7c7630ae09fe96d34c2a471a7b259d4ba1cd
Kurtis Garcia <kurtisgarcia14@gmail.com>
2021-03-26 12:08:29 -0400

    added Navbar.jsx with lowercase b

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,13 @@
-import React from 'react';
-import * as ReactBootStrap from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
-function Navbar() {
-
+const NavBar = () => {
     return (
-
-        <ReactBootStrap.Navbar sticky="top" collapseOnSelect expand="xl" bg="dark" variant="dark">
-        <ReactBootStrap.Navbar.Brand href="/">PuppyLuv 🐶🐶</ReactBootStrap.Navbar.Brand>
-        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-        <ReactBootStrap.Nav className="mr-auto">
-            <ReactBootStrap.Nav.Link href="#features">Account Information</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="#pricing">My Friends</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.NavDropdown title="Quick Chat" id="collasible-nav-dropdown">
-            <ReactBootStrap.NavDropdown.Item href="/">Friend 1</ReactBootStrap.NavDropdown.Item>
-            <ReactBootStrap.NavDropdown.Item href="/">Friend 2</ReactBootStrap.NavDropdown.Item>
-            <ReactBootStrap.NavDropdown.Item href="/">Friend 3</ReactBootStrap.NavDropdown.Item>
-            <ReactBootStrap.NavDropdown.Item href="/">Friend 4</ReactBootStrap.NavDropdown.Item>
-            </ReactBootStrap.NavDropdown>
-        </ReactBootStrap.Nav>
-        <ReactBootStrap.Nav>
-            <ReactBootStrap.Nav.Link href="/">About The Developers</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link eventKey={2} href="/">
-            Log Out
-            </ReactBootStrap.Nav.Link>
-        </ReactBootStrap.Nav>
-        </ReactBootStrap.Navbar.Collapse>
-        </ReactBootStrap.Navbar>
-
-);
+        <nav className="navbar">
+            <div className="nav-links">
+                <Link to='/'>Home</Link>
+            </div>
+        </nav>
+    )
 }
 
-export default Navbar;
+export default NavBar;
