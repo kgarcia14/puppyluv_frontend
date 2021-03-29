236518cceb1e71f4dcb105ee767e6fe592efb16e
Susan Yun <susanhkyun@gmail.com>
2021-03-29 09:27:32 -0500

    styles3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import './App.css';
 import Profile from './components/Profile';
-import Navbar from './components/Navbar';
+import Header from './components/Header';
 
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url('https://i.pinimg.com/originals/98/93/03/989303cde8235b119332ff7af0961bcd.jpg')`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}))
 
 function App() {
   const { isLoading } = useAuth0();
