514bc66a5b7e3be558a01b41a8b1a9fd3d23381c
Kurtis Garcia <kurtisgarcia14@gmail.com>
2021-03-30 16:33:16 -0400

    took out second useState out of profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import JSONPretty from 'react-json-pretty';
-import { useState } from 'react';
 import Home from './Home';
 import UserInfo from './UserInfo';
 
