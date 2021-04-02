288f4bc11ea509eb82558584dabd3c4500da6fbb
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-04-02 16:38:11 -0400

    Refactored the possible connections to display only when the filter was not submitted

--- src/components/multiLevelSelect/Filter.jsx
+++ src/components/multiLevelSelect/Filter.jsx
@@ -7,174 +7,58 @@ import {
     gray5,
     accent1
 } from '../nonConnectedUsers/styles/Styles'
+import FilterThumbCard from './FilterThumbCard';
 
 const Container = Styled.div`
-  width: 400px;
-  margin: 30px auto;
-  font-size: ${fontSize};
-  color: ${gray2};
+width: 100%;
+margin: 30px auto;
+font-size: ${fontSize};
+color: ${gray2};
 `;
 
 const List = Styled.ul`
-  list-style: none;
-  padding: 0px 20px;
-  background-color: #fff;
-  border-bottom-left-radius: 4px;
-  border-bottom-right-radius: 4px;
-  border-top: 3px solid ${accent1};
-  box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+display: flex;
+flex-direction: column;
+align-items: center;
+list-style: none;
+padding: 0px 20px;
+background-color: #fff;
+border-bottom-left-radius: 4px;
+border-bottom-right-radius: 4px;
+border-top: 3px solid ${accent1};
+box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
 `;
 
 const ListItem = Styled.li`
-  display: flex;
-  flex-direction: column;
-  padding: 10px 0px;
-  border-top: 1px solid ${gray5};
-  :first-of-type {
-    border-top: none;
-  }
+padding: 10px 0px;
+width: 260px;
+border-top: 1px solid ${gray5};
+:first-of-type {
+  border-top: none;
+}
+@media (min-width: 370px) {
+  width: 315px;
+}
+@media (min-width: 410px) {
+  width: 345px;
+}
+@media (min-width: 750px) {
+  width: 445px;
+}
 `;
 
-const Filter = () => {
-    const [filter, setFilter] = useState('')
-    const [filterBy, setFilterBy] = useState('')
-    const [filteredUsers, setFilteredUsers] = useState([])
-    console.log("filter is: ", filter)
-    console.log("filterBy is: ", filterBy)
+const Filter = ({ filteredUsers }) => {
     console.log("filtered users are: ", filteredUsers);
-
-    const _handleFilterChange = (e) => {
-        setFilter(e.target.value);
-      };
-    const _handleFilterByChange = (e) => {
-        setFilterBy(e.target.value)   
-    }
-
-    const _handleFilterSubmit = async (e) => {
-        e.preventDefault();
-        setFilteredUsers([])
-        const apiUrl = `http://127.0.0.1:3333/filter/${filterBy}`;
-        const filteredData = await fetch(apiUrl).then(response => response.json());
-        setFilteredUsers(filteredData);
         
-    }
-            
     return(
         <>
-        <p>Filter</p>
-        <select
-        value={filter}
-        dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-        placeholder="Please select"
-        onChange={_handleFilterChange}>
-            <option value="">please choose</option>
-            <option value="age">Age</option>
-            <option value="city">City</option>
-            <option value="gender">Gender</option>
-            <option value="numb_pets">Number of Pets</option>
-            <option value="pet_personality">Pet Personality</option>
-        </select>
-        {filter === 'age' && (
-            <form onSubmit={_handleFilterSubmit}>
-                <select
-                value={filterBy}
-                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                placeholder="Please select"
-                onChange={_handleFilterByChange}>
-                    <option value="">please choose</option>
-                    <option value="1820">18 - 20</option>
-                    <option value="2025">20 - 25</option>
-                    <option value="2530">25 - 30</option>
-                    <option value="3035">30 - 35</option>
-                    <option value="3540">35 - 40</option>
-                </select>
-                <button
-                    type="submit">
-                    Submit
-                </button>
-            </form>
-        )}
-        {filter === 'gender' && (
-            <form onSubmit={_handleFilterSubmit}>
-                <select
-                value={filterBy}
-                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                placeholder="Please select"
-                onChange={_handleFilterByChange}>
-                    <option value="">please choose</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    <option value="other">Other</option>
-                </select>
-                <button
-                    type="submit">
-                    Submit
-                </button>
-            </form>
-        )}
-        {filter === 'city' && (
-            <form onSubmit={_handleFilterSubmit}>
-                <select
-                value={filterBy}
-                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                placeholder="Please select"
-                onChange={_handleFilterByChange}>
-                    <option value="">please choose</option>
-                    <option value="Atlanta">Atlanta</option>
-                </select>
-                <button
-                    type="submit">
-                    Submit
-                </button>
-            </form>
-        )}
-        {filter === 'numb_pets' && (
-            <form onSubmit={_handleFilterSubmit}>
-                <select
-                value={filterBy}
-                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                placeholder="Please select"
-                onChange={_handleFilterByChange}>
-                    <option value="">please choose</option>
-                    <option value="0">No Pets</option>
-                    <option value="1">1 Pet</option>
-                    <option value="2">2 Pets</option>
-                    <option value="3">3 Pets</option>
-                    <option value="4">3+ Pets</option>
-                </select>
-                <button
-                    type="submit">
-                    Submit
-                </button>
-            </form>
-        )}
-        {filter === 'pet_personality' && (
-            <form onSubmit={_handleFilterSubmit}>
-                <select
-                value={filterBy}
-                dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
-                placeholder="Please select"
-                onChange={_handleFilterByChange}>
-                    <option value="">please choose</option>
-                    <option value="playful">Playful</option>
-                    <option value="outgoing">Outgoing</option>
-                    <option value="mellow">Mellow</option>
-                    <option value="independent">Independent</option>
-                    <option value="adaptable">Adaptable</option>
-                    <option value="grumpy">Grumpy</option>
-                </select>
-                <button
-                    type="submit">
-                    Submit
-                </button>
-            </form>
-        )}
+
         {!!filteredUsers ? (
             <Container>
                 <List>
                 {filteredUsers.map((fUser, index) => (
                     <ListItem key={index}>
-                        <p>{fUser.first_name}</p>
+                        <FilterThumbCard fUser={fUser} />
                     </ListItem>
                 ))}
                 </List>
--- src/components/nonConnectedUsers/styles/Styles.jsx
+++ src/components/nonConnectedUsers/styles/Styles.jsx
@@ -2,7 +2,7 @@ export const gray1 = "#383737";
 export const gray2 = "#565555";
 export const gray3 = "#857c81";
 export const gray4 = "#b9b9b9";
-export const gray5 = "#e0dddd";
+export const gray5 = "#9ee4f9";
 
 // Colors
 export const primary1 = "#6ca583";
