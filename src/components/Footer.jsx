92ffb4e23476c96c9a1eea3a2099d4f7df715621
Susan Yun <susanhkyun@gmail.com>
2021-04-05 16:01:05 -0500

    added bottom nav in mobile view, fixed up a bit of my profile page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
+import React from 'react';
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import { Typography, Link } from "@material-ui/core";
 import classNames from 'classnames'
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        borderTop: 'solid 3px #998643',
-        paddingTop: '12px',
         overflowX: 'hidden',
         fontFamily: 'Cabin',
         fontSize: '10px',
@@ -40,58 +43,96 @@ const useStyles = makeStyles((theme) => ({
     },
     flexContainer: {
         display: 'flex'
+    },
+    sectionDesktop: {
+        paddingTop: '12px',
+        borderTop: 'solid 3px #998643',
+        display: 'none',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        [theme.breakpoints.up('md')]: {
+            display: 'block',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        backgroundColor: '#ffb6c1',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+    bottomNav: {
+        backgroundColor: "transparent",
+        width: '100%',
     }
 }));
 
 export default function Footer() {
     const classes = useStyles();
     const currentYear = new Date().getFullYear()
-    
-    return (
-    <div className={classes.root}>
-    <Grid container spacing={0} className={classNames(classes.footerText, classes.footerSections)}>
-        <Grid item xs>
-            <div>
-            <span property="name">Puppy Luv Inc.</span>
-            <div property="address" typeof="PostalAddress">
-            <span property="streetAddress">10101 Cookie Coder St.</span>
-            <span property="addressLocality" style={{ display: 'block' }}>Atlanta, Georgia{''}</span>
-            </div>
-            </div>
-        </Grid>
-        <Grid item xs>
-            <ul style={{ listStyle: 'none', margin: 0 }}>
-            <li>
-                <Link prefetch={true} as="/github"
-                href="https://github.com/kgarcia14/puppyluv_frontend">
-                <a className={classes.white} style={{ fontWeight: 400 }}>GitHub</a>
-                </Link>
-            </li>
-            <li>
-                <Link prefetch={true} as="/thewriteupthingamajiggg"
-                href="/page?type=aboutPages&id=ferpa">
-                <a className={classes.white} style={{ fontWeight: 400 }}>Documentation</a>
-                </Link>
-            </li>
-            <li>
-                <Link prefetch={true} as="/directions-and-attractions"
-                href="/page?type=aboutPages&id=directions-and-attractions">
-                <a className={classes.white} style={{ fontWeight: 400 }}>About Us</a>
-                </Link>
-            </li>
-            </ul>
-        </Grid>
-    </Grid>
+    const [value, setValue] = React.useState(0);
 
-        <Grid className={classes.subFooter} item xs={12}>
-        <Typography
-            className={classes.white}
-            variant="subheading"
-            component={'span'}
-        >
-            © {currentYear} PuppyLuv Inc.
-        </Typography>
-        </Grid>
-    </div>
+    return (
+        <div className={classes.root}>
+            <div className={classes.sectionDesktop}>
+                <Grid container spacing={0} className={classNames(classes.footerText, classes.footerSections)}>
+                    <Grid item xs>
+                        <div>
+                            <span property="name">Puppy Luv Inc.</span>
+                            <div property="address" typeof="PostalAddress">
+                                <span property="streetAddress">10101 Cookie Coder St.</span>
+                                <span property="addressLocality" style={{ display: 'block' }}>Atlanta, Georgia{''}</span>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs>
+                        <ul style={{ listStyle: 'none', margin: 0 }}>
+                            <li>
+                                <Link prefetch={true} as="/github"
+                                    href="https://github.com/kgarcia14/puppyluv_frontend">
+                                    <a className={classes.white} style={{ fontWeight: 400 }}>GitHub</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link prefetch={true} as="/thewriteupthingamajiggg"
+                                    href="/page?type=aboutPages&id=ferpa">
+                                    <a className={classes.white} style={{ fontWeight: 400 }}>Documentation</a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link prefetch={true} as="/directions-and-attractions"
+                                    href="/page?type=aboutPages&id=directions-and-attractions">
+                                    <a className={classes.white} style={{ fontWeight: 400 }}>About Us</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </Grid>
+                </Grid>
+
+                <Grid className={classes.subFooter} item xs={12}>
+                <Typography
+                    className={classes.white}
+                    variant="subheading"
+                    component={'span'}
+                >
+                    © {currentYear} PuppyLuv Inc.
+                </Typography>
+                </Grid>
+            </div>
+
+            <div className={classes.sectionMobile}>
+                <BottomNavigation
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                    showLabels
+                    className={classes.bottomNav}
+                >
+                    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+                    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+                </BottomNavigation>
+            </div>
+            </div>
     )
 }
--- src/components/profileStuff/MyProfile.jsx
+++ src/components/profileStuff/MyProfile.jsx
@@ -1,49 +1,136 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { useState, useEffect } from 'react';
-import PossibleConnections from '../nonConnectedUsers/PossibleConnections';
-import PetRegister from '../petRegistry/PetRegister';
-import UserRegister from './UserRegister';
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        display: "flex",
+        display: "block",
         justifyContent: "center",
         alignItems: "center",
         fontFamily: "Cabin",
-        height: '50vh',
-        textAlign: 'center',
-        backgroundColor: 'pink',
-        marginLeft: '30px',
+        textAlign: "center",
+        backgroundColor: "pink",
+        marginLeft: "30px",
+        marginRight: "30px",
+        marginBottom: "30px",
+        marginTop: "30px",
+        paddingBottom: '30px',
+        minHeight: '100px',
+    },
+    header: {
+        paddingTop: "30px",
+    },
+    profile: {
+        flexGrow: 1,
+        paddingLeft: '50px',
+        paddingRight: '50px',
+        display: 'flex',
         marginRight: '30px',
+        marginLeft: '30px',
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        border: '2px #e75480 solid',
     },
 }));
 
-const MyProfile = ({handleReload, reload}) => {
+const MyProfile = ({ handleReload, reload }) => {
     const classes = useStyles();
     const { user } = useAuth0();
     const [uniqueId, setUniqueId] = useState([]);
 
     useEffect(() => {
         (async () => {
-            console.log(user.sub)
-            const apiUrl = `http://127.0.0.1:3333/users/${user.sub}`
-            const usersData = await fetch(apiUrl).then(response => response.json());
-            setUniqueId(usersData)
+            console.log(user.sub);
+            const apiUrl = `http://127.0.0.1:3333/users/${user.sub}`;
+            const usersData = await fetch(apiUrl).then((response) => response.json());
+            setUniqueId(usersData);
         })();
-    }, [user.sub, reload])
+    }, [user.sub, reload]);
 
     return (
         <div className={classes.root}>
-            <header>
-                <div>
-                    {uniqueId.user_img}
-                </div>
-                <h1>{uniqueId.first_name} {uniqueId.last_name}</h1>
-                <p>{uniqueId.age} {uniqueId.gender} {uniqueId.city}</p>
+            <div className={classes.header}>
+                <div>{uniqueId.user_img}</div>
+                <h1>
+                    {uniqueId.first_name} {uniqueId.last_name}
+                </h1>
+                <p>
+                    {uniqueId.age} {uniqueId.gender} {uniqueId.city}
+                </p>
                 <p>{uniqueId.about_us}</p>
-                <p>hehe</p>
-            </header>
+            </div>
+            <h4>My Pets</h4>
+            <div className={classes.profile}>
+                    {!!uniqueId.pet_name1 ? (
+                    <Grid container spacing={3}>
+                        <Grid item xs>
+                            <Paper className={classes.paper}>
+                                <h4>{uniqueId.pet_name1}</h4>
+                                <p>{uniqueId.pet_breed1}, {uniqueId.pet_age1}</p>
+                                <p>{uniqueId.pet_personality1}</p>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                    ) : (
+                        <>
+                        </>
+                    )}
+
+                    {!!uniqueId.pet_name2 ? (
+                    <Grid container spacing={3}>
+                        <Grid item xs>
+                            <Paper className={classes.paper}>
+                                <h4>{uniqueId.pet_name1}</h4>
+                                <p>{uniqueId.pet_breed1}, {uniqueId.pet_age1}</p>
+                                <p>{uniqueId.pet_personality1}</p>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs>
+                            <Paper className={classes.paper}>
+                                <h4>{uniqueId.pet_name2}</h4>
+                                <p>{uniqueId.pet_breed2}, {uniqueId.pet_age2}</p>
+                                <p>{uniqueId.pet_personality2}</p>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                    ) : (
+                        <>
+                        </>
+                    )}
+
+                    {!!uniqueId.pet_name3 ? (
+                    <Grid container spacing={3}>
+                        <Grid item xs>
+                            <Paper className={classes.paper}>
+                                <h4>{uniqueId.pet_name1}</h4>
+                                <p>{uniqueId.pet_breed1}, {uniqueId.pet_age1}</p>
+                                <p>{uniqueId.pet_personality1}</p>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs>
+                            <Paper className={classes.paper}>
+                                <h4>{uniqueId.pet_name2}</h4>
+                                <p>{uniqueId.pet_breed2}, {uniqueId.pet_age2}</p>
+                                <p>{uniqueId.pet_personality2}</p>
+                            </Paper>
+                        </Grid>
+                        <Grid item xs>
+                            <Paper className={classes.paper}>
+                                <h4>{uniqueId.pet_name3}</h4>
+                                <p>{uniqueId.pet_breed3}, {uniqueId.pet_age3}</p>
+                                <p>{uniqueId.pet_personality3}</p>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                    ) : (
+                        <>
+                        </>
+                    )}
+            </div>
         </div>
     );
 };
